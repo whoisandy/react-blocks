1c44850c4d3af37d226e59ac1793eca8d455766e
Bhargav Anand <rjn143@gmail.com>
2015-06-22 20:50:11 -0400

    centered test added, flex none property tested

--- test/BlockSpec.js
+++ test/BlockSpec.js
@@ -31,12 +31,20 @@ describe('Block Spec: Basic', () => {
     expect(type).to.eql('ul');
   });
 
-  it('should have display flex and default flex attrs', () => {
+  it('should have display flex and flex attrs', () => {
     let block = TestUtils.renderIntoDocument(<Block layout flex/>);
     let elem = TestUtils.findRenderedDOMComponentWithTag(block, 'div');
     expect(elem.props.style.display).to.eql('flex');
     expect(elem.props.style.flexGrow).to.eql(1);
     expect(elem.props.style.flexShrink).to.eql(1);
+    expect(elem.props.style.flexBasis).to.eql('auto');
+  });
+
+  it('should have flex none', () => {
+    let block = TestUtils.renderIntoDocument(<Block />);
+    let elem = TestUtils.findRenderedDOMComponentWithTag(block, 'div');
+    expect(elem.props.style.flexGrow).to.eql(0);
+    expect(elem.props.style.flexShrink).to.eql(0);
     expect(elem.props.style.flexBasis).to.eql('auto');
   });
 });
@@ -170,6 +178,14 @@ describe('Block Spec: Wrap', () => {
 })
 
 describe('Block Spec: General', () => {
+  it('should be centered along both axes', () => {
+    let block = TestUtils.renderIntoDocument(<Block centered/>);
+    let elem = TestUtils.findRenderedDOMComponentWithTag(block, 'div');
+    expect(elem.props.style.display).to.eql('flex');
+    expect(elem.props.style.alignItems).to.eql('center');
+    expect(elem.props.style.justifyContent).to.eql('center');
+  });
+
   it('should be a block element', () => {
     let block = TestUtils.renderIntoDocument(<Block block />);
     let elem = TestUtils.findRenderedDOMComponentWithTag(block, 'div');
@@ -202,83 +218,3 @@ describe('Block Spec: General', () => {
     expect(elem.props.style.display).to.eql('none');
   });
 });
-
-// describe('Block Spec: Layout Test', () => {
-//   let block;
-//   beforeEach(() => {
-//     block = TestUtils.renderIntoDocument(
-//       <Block className="app">
-//         <Block className="sidebar" flex="200px" />
-//         <Block className="main" vertical>
-//           <Block className="header" flex="40px" />
-//           <Block className="content" />
-//         </Block>
-//       </Block>
-//     );
-//   });
-
-//   it('should have an app block with default flex styles', () => {
-//     let appElem = TestUtils.findRenderedDOMComponentWithClass(block, 'app');
-//     expect(appElem.props.style).to.deep.eql({
-//       display: 'flex',
-//       flexGrow: 1,
-//       flexBasis: 'auto',
-//       flexDirection: 'row',
-//       alignItems: 'stretch',
-//       justifyContent: 'flex-start',
-//       boxSizing: 'border-box'
-//     });
-//   });
-
-//   it('should have a sidebar block with flexBasis 200px', () => {
-//     let sidebarElem = TestUtils.findRenderedDOMComponentWithClass(block, 'sidebar');
-//     expect(sidebarElem.props.style).to.deep.eql({
-//       display: 'flex',
-//       flexGrow: 0,
-//       flexBasis: '200px',
-//       flexDirection: 'row',
-//       alignItems: 'stretch',
-//       justifyContent: 'flex-start',
-//       boxSizing: 'border-box'
-//     });
-//   });
-
-//   it('should have a main block with flexBasis auto', () => {
-//     let mainElem = TestUtils.findRenderedDOMComponentWithClass(block, 'main');
-//     expect(mainElem.props.style).to.deep.eql({
-//       display: 'flex',
-//       flexGrow: 1,
-//       flexBasis: 'auto',
-//       flexDirection: 'column',
-//       alignItems: 'stretch',
-//       justifyContent: 'flex-start',
-//       boxSizing: 'border-box'
-//     });
-//   });
-
-//   it('should have a header block with flexBasis 40px', () => {
-//     let mainElem = TestUtils.findRenderedDOMComponentWithClass(block, 'header');
-//     expect(mainElem.props.style).to.deep.eql({
-//       display: 'flex',
-//       flexGrow: 0,
-//       flexBasis: '40px',
-//       flexDirection: 'row',
-//       alignItems: 'stretch',
-//       justifyContent: 'flex-start',
-//       boxSizing: 'border-box'
-//     });
-//   });
-
-//   it('should have a content block with flexBasis auto', () => {
-//     let mainElem = TestUtils.findRenderedDOMComponentWithClass(block, 'content');
-//     expect(mainElem.props.style).to.deep.eql({
-//       display: 'flex',
-//       flexGrow: 1,
-//       flexBasis: 'auto',
-//       flexDirection: 'row',
-//       alignItems: 'stretch',
-//       justifyContent: 'flex-start',
-//       boxSizing: 'border-box'
-//     });
-//   });
-// });
