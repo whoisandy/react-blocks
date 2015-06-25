dc94cc2c1bf7e57f9068328d4e877f818e77ac3a
Bhargav Anand <rjn143@gmail.com>
2015-06-25 00:24:18 -0400

    moved production build webpack to root

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+
+var config = {
+  entry: './src/index.js',
+
+  output: {
+    library: 'ReactBlocks',
+    libraryTarget: 'umd'
+  },
+
+  resolve: {
+    extensions: ['', '.js'],
+    modulesDirectories: ['node_modules', './src']
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+      }
+    ]
+  },
+
+  externals: [
+    {
+      "react": {
+        root: "React",
+        commonjs2: "react",
+        commonjs: "react",
+        amd: "react"
+      }
+    }
+  ],
+
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
+  ]
+};
+
+module.exports = config;
