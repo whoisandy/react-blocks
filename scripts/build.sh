20c643a7572d7e19cd24cfc6af231d295473a82e
Bhargav Anand <rjn143@gmail.com>
2015-06-25 00:27:26 -0400

    updated npm scripts

---
+++ scripts/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+babel=node_modules/.bin/babel
+webpack=node_modules/.bin/webpack
+build_dir=lib
+
+rm -rf $build_dir
+
+$babel ./src -d $build_dir --ignore "tests"
+
+NODE_ENV=production $webpack src/index.js $build_dir/umd/ReactBlocks.js
+NODE_ENV=production $webpack -p src/index.js $build_dir/umd/ReactBlocks.min.js
+
+printf "\nGzip: Minified size `gzip -c $build_dir/umd/ReactBlocks.min.js | wc -c | awk {'print $1/1000'}` bytes\n"
