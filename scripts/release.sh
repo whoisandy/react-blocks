197f0c4d2a9929a70b4b9d3d76d77bf851697b16
Bhargav Anand <rjn143@gmail.com>
2015-06-26 02:09:03 -0400

    modified:   scripts/release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,4 +7,6 @@ mkdir -p $website
 cp demo/index.html $website/index.html
 
 NODE_ENV=production $webpack -p --config demo/webpack.config.js
+ga -A $website
+gc -m 'built website'
 git subtree push --prefix $website origin gh-pages
