0d657d00a82633794fbefb4a659adbf1fe623482
Bhargav Anand <rjn143@gmail.com>
2015-06-26 02:02:14 -0400

    renamed tmp to website

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
 webpack=node_modules/.bin/webpack
-tmp_dir=tmp
+website=website
 
-mkdir -p $tmp_dir
-cp demo/index.html $tmp_dir/index.html
+mkdir -p $website
+cp demo/index.html $website/index.html
 
 NODE_ENV=production $webpack -p --config demo/webpack.config.js
-git subtree push --prefix $tmp_dir origin gh-pages
+git subtree push --prefix $website origin gh-pages
