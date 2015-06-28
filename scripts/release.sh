e884d2ffcb4756ac9e76e5142b4c08dd5392f501
Bhargav Anand <rjn143@gmail.com>
2015-06-27 21:49:54 -0400

    insert styles into index.html

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,10 +1,13 @@
 #!/bin/bash -e
 
 webpack=node_modules/.bin/webpack
+
+demo=demo
 website=website
 
+rm -rf $website
 mkdir -p $website
-cp demo/index.html $website/index.html
+sed "s/<!-- styles -->/<link rel=\"stylesheet\" href=\"bundle.css\">/" $demo/index.html > $website/index.html
 
 NODE_ENV=production $webpack -p --config demo/webpack.config.js
 git add -A $website
