6c9ab3e3bf3856e6ce1537cc50de571a88948882
Bhargav Anand <rjn143@gmail.com>
2015-07-19 22:11:34 -0400

    remove website directory after pushing changes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,5 +5,6 @@ website=website
 git add -A $website
 git commit -m 'deploying website'
 git subtree push --prefix $website origin gh-pages
+rm -rf $website
 
 echo "Deployed website: http://whoisandie.github.io/react-blocks"
