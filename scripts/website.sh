e170c850ba716feb3137568bcc48917c451b20d6
Bhargav Anand <rjn143@gmail.com>
2015-07-19 22:02:46 -0400

    keep deploy logic in a separate script

--- scripts/website.sh
+++ scripts/website.sh
@@ -7,15 +7,17 @@ webpack=node_modules/.bin/webpack
 demo=demo
 website=website
 
-echo "Deploying website..."
+echo "Building website..."
 
 rm -rf $website
 mkdir -p $website
 sed "s/<!-- styles -->/<link rel=\"stylesheet\" href=\"bundle.css\">/" $demo/index.html > $website/index.html
 
 NODE_ENV=production $webpack -p --config demo/webpack.config.js
-git add -A $website
-git commit -m 'built website'
-git subtree push --prefix $website origin gh-pages
+echo "Website built"
 
-echo "Deployed website: http://whoisandie.github.io/react-blocks"
+# git add -A $website
+# git commit -m 'built website'
+# git subtree push --prefix $website origin gh-pages
+#
+# echo "Deployed website: http://whoisandie.github.io/react-blocks"
