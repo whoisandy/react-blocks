6bf564adab1f5c4bda99eb83217e40fc59515669
Bhargav Anand <rjn143@gmail.com>
2015-09-26 19:25:50 -0700

    Chore: update build scripts

--- webpack.config.js
+++ webpack.config.js
@@ -2,47 +2,174 @@
 
 var path = require('path');
 var webpack = require('webpack');
+var merge = require('webpack-merge');
 
-var config = {
-  entry: './src/index.js',
+var HtmlPlugin = require('html-webpack-plugin');
 
-  output: {
-    library: 'ReactBlocks',
-    libraryTarget: 'umd'
-  },
+var TARGET = process.env.npm_lifecycle_event;
+var ROOT_PATH = path.resolve(__dirname);
+
+var config;
+var paths = {
+  src: path.join(ROOT_PATH, 'src'),
+  demo: path.join(ROOT_PATH, 'demo'),
+};
+
+var common = {
+  entry: path.resolve(paths.demo, 'index'),
 
   resolve: {
-    extensions: ['', '.js'],
-    modulesDirectories: ['node_modules', './src']
-  },
+    extensions: ['', '.js', '.less'],
+  }
+};
 
-  module: {
-    loaders: [
+if (TARGET === 'start' || !TARGET) {
+  config = start();
+} else if (TARGET === 'build' || !TARGET) {
+  config = build();
+} else if (TARGET === 'deploy' || !TARGET) {
+  config = deploy();
+}
+
+function start() {
+  const IP = '0.0.0.0';
+  const PORT = 3000;
+
+  return merge(common, {
+    ip: IP,
+    port: PORT,
+    devtool: 'eval-source-map',
+
+    entry: [
+      'webpack-dev-server/client?http://' + IP + ':' + PORT,
+      'webpack/hot/only-dev-server',
+      path.join(paths.demo, 'index'),
+    ],
+
+    output: {
+      path: __dirname,
+      filename: 'bundle.js'
+    },
+
+    resolve: {
+      alias: {
+        'react-blocks$': paths.src,
+      },
+    },
+
+    module: {
+      preLoaders: [
+        {
+          test: /\.js$/,
+          loaders: ['eslint'],
+          include: [paths.demo, paths.src],
+        }
+      ],
+      loaders: [
+        {
+          test: /\.js?$/,
+          loaders: ['react-hot', 'babel?stage=0'],
+          include: [paths.demo, paths.src],
+        },
+        {
+          test: /\.less$/,
+          exclude: /node_modules/,
+          loaders: ['style', 'css', 'less'],
+        },
+      ]
+    },
+
+    plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('development'),
+      }
+    }),
+    new HtmlPlugin({
+      title: 'React Blocks',
+      inject: true,
+    }),
+    new webpack.NoErrorsPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    ],
+  });
+}
+
+function build() {
+  return merge(common, {
+    entry: path.resolve(paths.src, 'index'),
+
+    output: {
+      library: 'ReactBlocks',
+      libraryTarget: 'umd'
+    },
+
+    module: {
+      loaders: [
+        {
+          test: /\.js?$/,
+          exclude: /node_modules/,
+          loader: 'babel?stage=0',
+        }
+      ]
+    },
+
+    externals: [
       {
-        test: /\.js?$/,
-        exclude: /node_modules/,
-        loader: 'babel?stage=0',
+        "react": {
+          root: "React",
+          commonjs2: "react",
+          commonjs: "react",
+          amd: "react"
+        }
       }
     ]
-  },
+  });
+}
 
-  externals: [
-    {
-      "react": {
-        root: "React",
-        commonjs2: "react",
-        commonjs: "react",
-        amd: "react"
-      }
-    }
-  ],
+function deploy() {
+  return merge(common, {
+    entry: path.resolve(paths.demo, 'index'),
 
-  plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    })
-  ]
-};
+    output: {
+      path: 'deploy',
+      filename: 'bundle.js'
+    },
+
+    resolve: {
+      alias: {
+        'react-blocks$': paths.src,
+      },
+    },
+
+    module: {
+      loaders: [
+        {
+          test: /\.js?$/,
+          exclude: /node_modules/,
+          loaders: ['babel?stage=0'],
+        },
+        {
+          test: /\.less$/,
+          exclude: /node_modules/,
+          loaders: ['style', 'css', 'less']
+        }
+      ]
+    },
+
+    plugins: [
+      new HtmlPlugin({
+        title: 'React Blocks',
+        inject: true,
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        }
+      }),
+      new webpack.optimize.OccurenceOrderPlugin(),
+    ]
+  });
+}
 
 module.exports = config;
